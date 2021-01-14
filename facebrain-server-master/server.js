ee765969b8374489ac950c24d039108f85ed8e86
ardyutama <ardy.putrautama53@gmail.com>
2021-01-14 18:25:58 +0700

    abw

---
+++ facebrain-server-master/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const formData = require('express-form-data');
+
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(formData.parse());
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+    	user : 'postgres',
+    	password : 'ardy1607',
+    	database : 'projectbaru',
+	}
+});
+
+app.get("/", (req, res) => {
+	res.send("welcome");
+});
+
+app.post("/signin", signin.handleSignIn(db, bcrypt));
+
+app.post("/register", register.handleRegister(db, bcrypt));
+
+app.get("/profile/:id", profile.handleProfile(db));
+
+app.put("/image", image.handleImage(db));
+app.post("/imageurl", image.handleApiCall());
+app.post("/image-upload", image.handleImageUpload());
+
+app.get("*", (req, res) => {
+	res.send("sorry, nothing here((");
+});
+
+app.listen(process.env.PORT || 3001, function(){
+	console.log("server starts");
+});
